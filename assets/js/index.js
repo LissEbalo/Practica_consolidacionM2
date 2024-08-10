2b7e3d74d887272c0bc3e84d79b6dda43062eca4
Lissette Lillo <xlissx@gmail.com>
2024-08-09 23:16:12 -0400

    add1

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// document.getElementById('boton_enviar').onclick = function(){
+//     alert('Gracias por contactarme');
+// }
+document.getElementById("contact_form").addEventListener('submit', (e)=>{
+    e.preventDefault();
+    alert('Gracias por contactarme')
+    // document.getElementById("contact_form").style.display = "none"
+   }
+)
