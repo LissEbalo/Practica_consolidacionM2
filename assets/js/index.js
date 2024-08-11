5865751fec731f43086d7bb23344adf787eac6a7
Lissette Lillo <xlissx@gmail.com>
2024-08-11 15:10:55 -0400

    ultimos ajustes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+$ (document).ready(function() {
+    $("#info_personal").text("Hola! Mi nombre es Lissette Beatriz Lillo Bucarey, soy de la comuna de Maipu, Chile, tengo 38 años. Soy estudiante en un Bootcamp de desarrollo Front-End. Mi meta es terminar este curso, encontrar un trabajo relacionado a mis estudios y volver a estudiar en otro bootcamp, para lograr la carrera de diseñadora UX/UI, comencé realizando cursos de desarrollo de videojuegos, avanzando en esos cursos es que llegue al área de programación, la cual me gustó mucho, al averiguar sobre el tema conocí el diseño UX/UI y fue algo que me encantó, así que voy por ese camino.");
+})
+
 // document.getElementById('boton_enviar').onclick = function(){
 //     alert('Gracias por contactarme');
 // }
